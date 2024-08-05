34965d2227403a757c033a9a534b7e06fd2a5827
m-pihlak <pihlakmer@gmail.com>
2024-08-05 20:45:22 +0300

    Now dedicated to finding apriltags

--- apriltag_detector.py
+++ apriltag_detector.py
@@ -1,7 +1,6 @@
 from pupil_apriltags import Detector
 import cv2
 
-
 """
 Changes the AprilTag detector's used family.
 """
@@ -33,8 +32,7 @@ A(int(x), int(y)),
 B(int(x), int(y)), 
 C(int(x), int(y)),
 ...              , 
-N(int(x), int(y)),
-A(int(x), int(y))
+N(int(x), int(y))
 """
 def calculate_corners(result):
     corners = []
@@ -63,26 +61,12 @@ def write_text(image, X, Y, text):
                 (0, 255, 0), 2)
 
 """
-Draws a border around the AprilTags in the given image.
+Finds AprilTags in image.
 Uses the detector created with create_detector() and 
 detects AprilTags from the family defined with use_family(family),
 or "tag36h11", if not defined.
 """
-def draw_corners_around_image(image):
+def find_apriltags(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     results = detector.detect(gray)
-
-    for result in results:
-        corners = calculate_corners(result)
-
-        for i, corner1 in enumerate(corners[:-1]):
-            corner2 = corners[i + 1]
-            cv2.line(image, 
-                     corner1, corner2,
-                    (0, 255, 0), 2)
-        
-        centerX, centerY = int(result.center[0]), int(result.center[1])
-        draw_circle(image, centerX, centerY)
-
-        tagFamily = result.tag_family.decode("utf-8")
-        write_text(image, centerX, centerY, tagFamily)
+    return results
